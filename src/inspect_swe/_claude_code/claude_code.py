c4815342760729a4987635fb89d795a2bd87885d
J.J. Allaire <jj.allaire@gmail.com>
2025-08-30 10:30:04 -0400

    not yet implemented

--- src/inspect_swe/_claude_code/claude_code.py
+++ src/inspect_swe/_claude_code/claude_code.py
@@ -4,6 +4,6 @@ from inspect_ai.agent import Agent, AgentState, agent
 @agent
 def claude_code() -> Agent:
     async def execute(state: AgentState) -> AgentState:
-        return state
+        raise RuntimeError("claude_code() not yet implemented.")
 
     return execute
