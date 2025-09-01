bfe82739f567cf9c229e37c0c47eae44e89ce432
J.J. Allaire <jj.allaire@gmail.com>
2025-09-01 08:07:02 -0400

    formatting and docs

--- examples/web_search/task.py
+++ examples/web_search/task.py
@@ -1,8 +1,7 @@
 from inspect_ai import Task, task
 from inspect_ai.dataset import Sample
 from inspect_ai.tool import MCPServerConfigStdio
-from inspect_swe import claude_code
-from inspect_swe._claude_code.claude_code import ClaudeCodeOptions
+from inspect_swe import ClaudeCodeOptions, claude_code
 
 
 @task
@@ -10,12 +9,15 @@ def web_search() -> Task:
     return Task(
         dataset=[
             Sample(
-                input="What transport protocols are supported in the 2025-03-26 version of the MCP spec?"
+                input="What transport protocols are supported in "
+                + " the 2025-03-26 version of the MCP spec?"
             )
         ],
         solver=claude_code(
             options=ClaudeCodeOptions(
-                system_prompt="Please use the WebSearch tool to research this question and the memory tools to keep track of your research.",
+                system_prompt="Please use the WebSearch tool to "
+                + "research this question and the memory tools "
+                + "to keep track of your research.",
                 mcp_servers=[
                     MCPServerConfigStdio(
                         name="memory",
