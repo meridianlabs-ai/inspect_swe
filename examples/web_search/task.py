0b646ac14978921f70478866ab8484e61e00f795
J.J. Allaire <jj.allaire@gmail.com>
2025-09-01 07:32:07 -0400

    add web_search example

---
+++ examples/web_search/task.py
@@ -0,0 +1,29 @@
+from inspect_ai import Task, task
+from inspect_ai.dataset import Sample
+from inspect_ai.tool import MCPServerConfigStdio
+from inspect_swe import claude_code
+from inspect_swe._claude_code.claude_code import ClaudeCodeOptions
+
+
+@task
+def web_search() -> Task:
+    return Task(
+        dataset=[
+            Sample(
+                input="What transport protocols are supported in the 2025-03-26 version of the MCP spec?"
+            )
+        ],
+        solver=claude_code(
+            options=ClaudeCodeOptions(
+                system_prompt="Please use the WebSearch tool to research this question and the memory tools to keep track of your research.",
+                mcp_servers=[
+                    MCPServerConfigStdio(
+                        name="memory",
+                        command="npx",
+                        args=["--offline", "@modelcontextprotocol/server-memory"],
+                    )
+                ],
+            )
+        ),
+        sandbox=("docker", "Dockerfile"),
+    )
