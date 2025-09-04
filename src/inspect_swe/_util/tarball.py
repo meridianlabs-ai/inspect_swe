c9877a7b19ab95fd80251204e5dbff64f346595e
J.J. Allaire <jj.allaire@gmail.com>
2025-09-04 14:27:48 -0400

    use dev version of inspect_ai

--- src/inspect_swe/_util/tarball.py
+++ src/inspect_swe/_util/tarball.py
@@ -8,7 +8,7 @@ def extract_tarball(tarball_bytes: bytes) -> bytes:
     # Open the gzip-compressed tarball
     with BytesIO(tarball_bytes) as tarball_io:
         with gzip.open(tarball_io, "rb") as gz:
-            with tarfile.open(fileobj=gz, mode="r") as tar:  # type: ignore[call-overload]
+            with tarfile.open(fileobj=gz, mode="r") as tar:
                 # List all members (should be just one file)
                 members = tar.getmembers()
                 if len(members) != 1:
